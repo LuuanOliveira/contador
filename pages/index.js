595dfeb4c58ffddd38a9df08203d3e9be9d61f15
Luan Oliveira <victorluan17@hotmail.com>
2020-11-09 19:51:07 -0300

    learning next.js :bulb:

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+function Home () {
+    return (
+        <div>
+            <h1>Contagem: </h1> 
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    function zerarContador() {
+        setContador(contador - contador);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+            <button onClick={zerarContador}>Zerar</button>
+        </div>
+    )
+}
+
+export default Home
